0f87bc0f5b7f0ed4abee55f489b584a0ef593fc6
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-10-13 02:46:21 +0200

    integration test in App..

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -1,44 +1,60 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
-import Home from "../Pages/Home/Home";
-import AuthLogin from "../Pages/Auth/AuthLogin";
 import userEvent from "@testing-library/user-event";
 
-describe("App", () => {
+describe("Testing App", () => {
   it("should render title Päron AB", () => {
     render(<App />);
 
-    expect(screen.getByRole("heading", "Päron Ab"));
+    expect(
+      screen.getByRole("heading", { name: "päron ab" })
+    ).toBeInTheDocument();
   });
 
-  it("should display Login Component when clicken on 'Login-button', Login-Link", async () => {
+  it("should display Login Component when clicken on 'Login-button' on the top", async () => {
     render(<App />);
 
-    const user = userEvent;
+    const user = userEvent.setup();
     const LogIn = screen.getByRole("link", "Logga");
     expect(LogIn).toBeInTheDocument();
 
-    user.click(LogIn);
+    await user.click(LogIn);
 
-    // await waitFor(() => {
-    //   expect(screen.getByRole("textbox", "email")).toBeInTheDocument();
-    //   expect(screen.getByRole("textbox", "password")).toBeInTheDocument();
-    //   expect(screen.getByRole("button", "logga in")).toBeInTheDocument();
-    // });
+    expect(screen.getByPlaceholderText("email")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("password")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "logga in" })
+    ).toBeInTheDocument();
+    expect(screen.getByText("skapa ett konto")).toBeInTheDocument();
+  });
+
+  it("should be able to login and se 'Lagersaldo-component' ", async () => {
+    render(<App />);
+
+    const user = userEvent.setup();
+    const LogIn = screen.getByRole("link", "Logga");
+    expect(LogIn).toBeInTheDocument();
+
+    await user.click(LogIn);
+
+    const inputLogin = screen.getByPlaceholderText("email");
+    await user.type(inputLogin, "nicoleavila@paron.se");
+
+    const inputPassword = screen.getByPlaceholderText("password");
+    await user.type(inputPassword, "123456");
+
+    const loginButton = screen.getByRole("button", { name: "logga in" });
+
+    await user.click(loginButton);
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("button", { name: "Logga ut" })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("heading", { name: "Lagersaldo" })
+      ).toBeInTheDocument();
+    });
   });
 });
-
-// it("should navigate", () => {
-//     //     const entries = "/paronAB/";
-
-//     //     <MemoryRouter initialEntries={[entries]}>
-//     //     <Routes>
-//     //       <Route path="/paronAB/" element={<Home />} />
-//     //       <Route path="/paronAB/auth" element={<Auth />} />
-//     //     </Routes>
-//     //   </MemoryRouter>
-//     render(<App />);
-//     const user = userEvent;
-//   });
